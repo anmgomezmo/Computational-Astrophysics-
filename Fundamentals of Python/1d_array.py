8e0d81502db5e06bf6b6bb0db69ecaaeb88fc057
Andres M. Gomez M <anmgomezmo@unal.edu.co>
2020-08-27 15:32:25 -0500

    Example of 1 dimentional array

---
+++ Fundamentals of Python/1d_array.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+a = np.array([0., 2., 4., 6.])
+print(a)
+
+b = np.arange(10)
+print(b)
+
+c = np.zeros(10, float)
+print(c)
+
+d = np.ones(10)
+print(d)
+
+e = np.linspace(1., 20., 9)
+print(e)
+
+f = np.random.random(10)
+print(f)
