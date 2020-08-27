c3faecb5fc112f4658d68e3a6f174afd32be1894
Andres M. Gomez M <anmgomezmo@unal.edu.co>
2020-08-27 14:19:41 -0500

    Added example of importing modules

---
+++ Fundamentals of Python/importing_modules.py
@@ -0,0 +1,7 @@
+import math
+
+x = math.pi
+print(f"An approximate value of pi is {x:.5f}")
+print("An approximate value of pi is {:.5f}".format(x))
+print("An approximate value of pi is %5.5f"%x)
+ # %5.5f 5 is the number of decimal digits, .5f five digits of float type 
