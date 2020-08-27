09feca67ca6304e988b2b82bac39b13c00a20687
Andres M. Gomez M <anmgomezmo@unal.edu.co>
2020-08-27 15:00:54 -0500

    example of functions

---
+++ Fundamentals of Python/simple_functions.py
@@ -0,0 +1,20 @@
+import math as m
+x = m.pi / 2.0
+print(m.sin(x))
+print(m.cos(x))
+
+x = m.pi / 2
+print(m.cos(x))
+
+a = 2.0
+b = 3.0
+c = a + b
+print(c)
+
+print(a/b)
+
+print(m.sqrt(5*c))
+
+print(m.exp(10.))
+
+print(m.log(22026.47))
