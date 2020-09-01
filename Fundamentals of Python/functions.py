5f550332cf5a76519a03dadf2f5f3bb866bcb378
Andres M. Gomez M <anmgomezmo@unal.edu.co>
2020-09-01 11:34:30 -0500

    Added function with description

--- Fundamentals of Python/functions.py
+++ Fundamentals of Python/functions.py
@@ -39,3 +39,28 @@ def myfunction4(x, y, z = 5):
 print(myfunction4(1., 2.))
 print(myfunction4(1., 2., 3.))
 print(myfunction4(y = 2., x = 2., z = 0.))
+
+print("\n\n")
+
+def LorentzGamma(v):
+    '''
+    --------------------------------------
+    LorentzGamma(v)
+    --------------------------------------
+    Receives a speed v in units of m/s
+    and returns the corresponding value
+    of the Lorentz gamma function in
+    special relativity.
+    --------------------------------------
+    '''
+    import numpy as np
+    c = 3E8 # speed of light in m/s
+    g = 1/np.sqrt(1-(v**2)/(c**2))
+    return g
+
+print(LorentzGamma(0))
+print(LorentzGamma(30000))
+print(LorentzGamma.__doc__)
+# print(LorentzGamma?)
+
+print("\n\n")
