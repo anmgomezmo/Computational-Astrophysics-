e93965f5eeccbcd8ac92a40383d7f062b1981ae7
Andres M. Gomez M <anmgomezmo@unal.edu.co>
2020-09-02 12:56:09 -0500

    Added examples of use of units of astropy

--- Fundamentals of Python/using_astropy.py
+++ Fundamentals of Python/using_astropy.py
@@ -16,4 +16,56 @@ rS = 2*G*M/c**2
 print(rS)
 rS = rS.decompose()
 print(rS)
-print(rS.to(u.km))
+print(rS.to(u.km), "\n\n")
+
+ly = 1*u.lyr  # light year (distance travelled by light in vacuum in one julian year)
+print(ly, "\n", ly.to(u.m), "\n\n")
+
+au = 1*u.au
+print(au, "\n", au.to(u.m), "\n", au.to(u.lyr), "\n\n")
+
+def d(alpha = 1*u.arcsec, baseline = 1*u.au):
+    '''
+    -------------------------------------------
+    d(alpha, baseline)
+    -------------------------------------------
+    Returns the distance to a star using the
+    parallax method.
+
+    Arguments:
+    baseline in au
+    parallax angle alpha given in arcsec
+    -------------------------------------------
+    '''
+    return baseline/np.tan(alpha)
+
+alphaCentauri_distance = d(0.75481*u.arcsec)
+print(alphaCentauri_distance, "\n", alphaCentauri_distance.to(u.lyr))
+
+pc = d(1*u.arcsec)
+print(pc, "\n", pc.to(u.pc), "\n", pc.to(u.lyr), "\n")
+print(d(1*u.marcsec).to(u.pc), "\n\n", alphaCentauri_distance.to(u.pc))
+
+cygni_distance = d(0.316*u.arcsec)
+print(cygni_distance, "\n", cygni_distance.to(u.pc), "\n\n")
+
+from sympy import Symbol, solve
+b = Symbol("b")
+print(solve(100.-b**5, b), "\n\n") #la solucion es el primer valor del vector resultado
+
+def M(m, D):
+    '''
+    -------------------------------------------
+    M(m, D)
+    -------------------------------------------
+    Returns the absolute magnitude of a star.
+
+    Arguments:
+    m: apparent magnitude
+    D: Distance to the sun in parsecs
+    -------------------------------------------
+    '''
+    return m - 5*(np.log10(D) - 1)
+
+D_sun = 1*u.au.to(u.pc)
+print(M(-26.83, D_sun))
