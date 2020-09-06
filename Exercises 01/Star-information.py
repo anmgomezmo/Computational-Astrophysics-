229a926875fa7b486d2651231a7412a3059004be
Andres M. Gomez M <anmgomezmo@unal.edu.co>
2020-09-06 18:30:21 -0500

    At last I've found how to read the string column

--- Exercises 01/Star-information.py
+++ Exercises 01/Star-information.py
@@ -1,10 +1,29 @@
 import numpy as np
 
-infile = open("stars_data.txt", "r")
-indata = infile.readlines()
-infile.close()
+#infile = open("stars_data.txt", "r")
+#indata = infile.readlines()
+#infile.close()
 
 
-n = len(indata)-1
-for i in range(n):
-    print(indata[i], "\n")
+#n = len(indata)-1
+#for i in range(n):
+    #print(indata[i], "\n")
+
+#Starname = np.array(n)
+#Stardistance = np.zeros(n)
+#Starluminosity = np.zeros(n)
+
+#for i in range(n):
+#    splitline = indata[i+1][:-1].split(",")
+#    Starname[i] = splitline[0]
+#    Stardistance[i] = float(splitline[1])
+#    Starluminosity[i] = float(splitline[3])
+
+
+#print(Starname, "\n", Stardistance, "\n", Starluminosity, "\n\n")
+
+data1 = np.loadtxt("stars_data.txt", comments = "#",
+delimiter = ",", usecols = [1,2,3])
+data2 = np.genfromtxt("stars_data.txt", comments = "#",
+delimiter = ",", usecols = [0], dtype = "str")
+print(data1, "\n", data2)
