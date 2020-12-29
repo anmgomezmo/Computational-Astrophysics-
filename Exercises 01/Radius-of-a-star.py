061aa710eacaafdf0acaaea4fb6212492320666d
Andres M. Gomez Mongui <anmgomezmo@unal.edu.co>
2020-12-28 20:36:38 -0500

    Return to the exercices of solar astrophysics course

--- Exercises 01/Radius-of-a-star.py
+++ Exercises 01/Radius-of-a-star.py
@@ -2,15 +2,20 @@ import numpy as np
 from astropy import constants as const
 from astropy import units as u
 
-def RadiusStar(L,T):
+def RadiusStar(L,T,watts=False,celcius=False):
     '''
     --------------------------------------
     RadiusStar(L,T)
     --------------------------------------
-    L: Luminocity in solar units
+    L: Luminocity adimentionaless by solar units
     T: Effective temperature in kelvin
     --------------------------------------
     '''
+    if watts:
+        L/=const.L_sun
+    if celcius:
+        T+= 273.15
+
     if L > 0 and T > 0:
         R = np.sqrt((L*const.L_sun)/
         (4*np.pi*const.sigma_sb*((T*u.k)**4)*const.R_sun**2))
@@ -30,3 +35,5 @@ print(RadiusStar(0.0017,-3042))
 print(RadiusStar(-0.0017,3042))
 
 print(RadiusStar(-0.0017,-3042))
+
+print(RadiusStar(6.506e23,2768.85,watts=True,celcius=True))
--- Exercises 01/Star-information.py
+++ Exercises 01/Star-information.py
@@ -22,8 +22,37 @@ import numpy as np
 
 #print(Starname, "\n", Stardistance, "\n", Starluminosity, "\n\n")
 
-data1 = np.loadtxt("stars_data.txt", comments = "#",
-delimiter = ",", usecols = [1,2,3])
-data2 = np.genfromtxt("stars_data.txt", comments = "#",
-delimiter = ",", usecols = [0], dtype = "str")
+
+def List_Order_Distance(array1,array2):
+    outfile = open('List_Order_Distance.txt','w')
+    headerstring = '# List of stars sorted by distance from the earth'
+    outfile.write(headerstring)
+
+    N = 11
+    List = array2[:,0]
+    for m in range(0,N):
+        for n in range(0,N-1):
+            d = List[n]
+            d_f = List[n+1]
+            if d<=d_f:
+                List[n] = d
+                List[n+1] = d_f
+            else:
+                List[n] = d_f
+                List[n+1] = d
+
+            aux = List[n]
+            if m==N-1:
+                outstring = f'\n {aux:.2f}'
+                outfile.write(outstring)
+
+    outfile.close()
+    return List
+
+
+
+data1 = np.genfromtxt("stars_data.txt", comments = "#", delimiter = ",", usecols = [0], dtype = "str")
+data2 = np.loadtxt("stars_data.txt", comments = "#", delimiter = ",", usecols = [1,2,3])
 print(data1, "\n", data2)
+print(data1.shape,data2.shape)
+print(List_Order_Distance(data1,data2))
